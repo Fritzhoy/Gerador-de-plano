627777233228649fec120ed7d76c984f866e4b1c
Fritzhoy <camila.gmarques2@gmail.com>
2025-04-08 16:34:52 -0300

    Update GERADOR_DE_PLANO.py

--- exercicio/GERADOR_DE_PLANO.py
+++ exercicio/GERADOR_DE_PLANO.py
@@ -1,15 +1,31 @@
 
-#Caso 2: "init": ["computador-com-problema", "checar-computador-liga", "a-bios-nao-inicia", "Speaker-emite-alerta"]
-#Caso 3: "init": ["computador-com-problema", "Computador-nao-liga"]
-
-
-
 from gps import gps
 
 problem = {
-    #Regra 4: 
-	 "init": ["computador-com-problema", "computador-liga", "a-bios-inicia", "sistema-operacional-nao-inicia", "unidade-de-armazenamento-detectada", "ordem-de-boot-correta", "unidade-de-armazenamento-nao-integra"],
+    #Regra1:
+    #"init": ["computador-com-problema", "computador-liga", "a-bios-inicia", "sistema-operacional-inicia", "sistema-operacional-inicia-completamente"],
+    #"finish": ["Operando-normalmente"],
+    
+	#Regra 2:
+    #"init": ["computador-com-problema", "computador-liga", "a-bios-inicia", "sistema-operacional-inicia", "sistema-operacional-nao-inicia-completamente"],
+    #"finish": ["sistema-operacional-corrompido"],
+    
+	#Regra 3: 
+    
+	#"init": ["computador-com-problema", "computador-liga", "a-bios-inicia", "sistema-operacional-inicia", "sistema-operacional-nao-inicia-completamente"],
+    #"finish": ["sistema-operacional-corrompido"],
+    
+	#Regra 4: 
+	"init": ["computador-com-problema", "computador-liga", "a-bios-inicia", "sistema-operacional-nao-inicia", "unidade-de-armazenamento-detectada", "ordem-de-boot-correta", "unidade-de-armazenamento-nao-integra"],
 	"finish": ["troque-o-HD/SDD"],
+    
+	#Regra 5: 
+    #"init": ["computador-com-problema", "computador-liga", "a-bios-inicia", "sistema-operacional-nao-inicia", "unidade-de-armazenamento-detectada", "ordem-de-boot-nao-esta-correta"],
+    #"finish": ["Ajustar-Boot"],
+
+	#Regra 6: 
+    #"init": ["computador-com-problema", "computador-liga", "a-bios-inicia", "sistema-operacional-nao-inicia", "unidade-de-armazenamento-nao-detectada"],
+    #"finish": ["HD-desconectado-ou-com-problema"],
     
 	#Regra 7
     #"init": ["computador-com-problema", "checar-computador-liga", "a-bios-nao-inicia", "Speaker-emite-alerta"],
@@ -19,18 +35,34 @@ problem = {
 	#"init": ["computador-com-problema", "computador-liga", "a-bios-nao-inicia", "Speaker-nao-emite-alerta"],
 	#"finish": ["problema-na-placa-mae"],
     
+	#Regra 9
+    #"init": ["computador-com-problema", "computador-nao-liga", "fonte-inicia", "voltagem-correta"],
+	#"finish": ["problema-no-circuito-da-placa-mae"],
+    
+	#Regra 10
+    #"init": ["computador-com-problema", "computador-nao-liga", "fonte-inicia", "voltagem-nao-esta-correta"],
+	#"finish": ["fonte-esta-com-problema"],
+    
+    #Regra 11
+    #"init": ["computador-com-problema", "computador-nao-liga", "fonte-nao-inicia", "alimentacao-conectada"],
+	#"finish": ["fonte-esta-queimada"],
+    
+	#Regra 12
+    #"init": ["computador-com-problema", "computador-nao-liga", "fonte-nao-inicia", "alimentacao-nao-conectada"],
+	#"finish": ["ligar-na-tomada"],
+    
 	"ops": [
     {
-		"action": "Sem-problemas",
-		"preconds": ["computador-com-problema","a-bios-inicia", "computador-liga", "sistema-operacional-inicia", "sistema-operacional-inicia-completamente"],
-		"add": ["Sem-problemas"],
-		"delete": ["computador-com-problema"]
+		"action": "sistema-operacional-inicia-completamente",
+		"preconds": ["computador-com-problema", "a-bios-inicia", "computador-liga", "sistema-operacional-inicia", "checar-sistema-operacional-inicia-completamente", "sistema-operacional-inicia-completamente"],
+		"add": ["Operando-normalmente"],
+		"delete": ["computador-com-problema", "checar-sistema-operacional-inicia-completamente"]
 	},
     {
-		"action": "sistema-operacional-corrompido",
-		"preconds": ["computador-com-problema", "a-bios-inicia", "computador-liga", "sistema-operacional-inicia", "sistema-operacional-nao-inicia-completamente"],
+		"action": "sistema-operacional-nao-inicia-completamente",
+		"preconds": ["computador-com-problema", "a-bios-inicia", "computador-liga", "sistema-operacional-inicia", "checar-sistema-operacional-inicia-completamente", "sistema-operacional-nao-inicia-completamente"],
 		"add": ["sistema-operacional-corrompido"],
-		"delete": ["computador-com-problema"]
+		"delete": ["computador-com-problema", "checar-sistema-operacional-inicia-completamente"]
 	},
     {
 		"action": "a-bios-inicia",
@@ -51,16 +83,34 @@ problem = {
 		"delete": ["checar-OS"]
 	},
     {
+		"action": "sistema-operacional-inicia",
+		"preconds": ["computador-com-problema", "computador-liga", "a-bios-inicia", "checar-OS", "sistema-operacional-inicia"],
+		"add": ["checar-sistema-operacional-inicia-completamente"],
+		"delete": ["checar-OS"]
+	},
+    {
 		"action": "unidade-de-armazenamento-detectada",
 		"preconds": ["computador-com-problema", "computador-liga", "a-bios-inicia", "sistema-operacional-nao-inicia", "unidade-de-armazenamento-detectada", "checar-unidade-armazenamento"],
 		"add": ["checar-ordem-boot"],
 		"delete": ["checar-unidade-armazenamento"]
 	},
     {
+		"action": "unidade-de-armazenamento-nao-detectada",
+		"preconds": ["computador-com-problema", "computador-liga", "a-bios-inicia", "sistema-operacional-nao-inicia", "unidade-de-armazenamento-nao-detectada", "checar-unidade-armazenamento"],
+		"add": ["HD-desconectado-ou-com-problema"],
+		"delete": ["computador-com-problema", "checar-unidade-armazenamento"]
+	},
+    {
 		"action": "ordem-de-boot-correta",
 		"preconds": ["computador-com-problema", "computador-liga", "a-bios-inicia", "sistema-operacional-nao-inicia", "unidade-de-armazenamento-detectada", "checar-ordem-boot", "ordem-de-boot-correta"],
 		"add": ["checar-unidade-de-armazenamento-integra"],
 		"delete": ["checar-ordem-boot"]
+	},
+    {
+		"action": "ordem-de-boot-nao-esta-correta",
+		"preconds": ["computador-com-problema", "computador-liga", "a-bios-inicia", "sistema-operacional-nao-inicia", "unidade-de-armazenamento-detectada", "checar-ordem-boot", "ordem-de-boot-nao-esta-correta"],
+		"add": ["Ajustar-Boot"],
+		"delete": ["computador-com-problema", "checar-ordem-boot"]
 	},
     {
 		"action": "unidade-de-armazenamento-nao-integra",
@@ -88,65 +138,47 @@ problem = {
 	},
     {
 		"action": "o-computador-nao-liga",
-		"preconds": ["computador-com-problema", "Computador-nao-liga"],
+		"preconds": ["computador-com-problema", "computador-nao-liga"],
 		"add": ["checar-fonte"],
 		"delete": [" "]
 	},
     {
-		"action": "a-fonte-inicia",
-		"preconds": ["Computador-nao-liga", "checar-fonte"],
-		"add": ["fonte-inicia", "checar-voltagem"],
+		"action": "fonte-inicia",
+		"preconds": ["computador-nao-liga", "checar-fonte","fonte-inicia"],
+		"add": ["checar-voltagem"],
 		"delete": ["checar-fonte"]
 	},
     {
-		"action": "a-fonte-nao-inicia",
-		"preconds": ["Computador-nao-liga", "fonte-nao-inicia"],
-		"add": ["checar-alimentcao"],
-		"delete": [" "]
+		"action": "fonte-nao-inicia",
+		"preconds": ["computador-com-problema", "computador-nao-liga", "checar-fonte",  "fonte-nao-inicia"],
+		"add": ["checar-alimentacao"],
+		"delete": ["checar-fonte"]
 	},
     {
-		"action": "Voltagem-esta-correta",
-		"preconds": ["computador-com-problema", "Computador-nao-liga", "fonte-inicia", "checar-voltagem", "Voltagem-correta"],
-		"add": ["checar-circuito-alimentacao"],
-		"delete": ["checar-voltagem"]
+		"action": "voltagem-correta",
+		"preconds": ["computador-com-problema", "computador-nao-liga", "fonte-inicia",  "checar-voltagem", "voltagem-correta"],
+		"add": ["problema-no-circuito-da-placa-mae"],
+		"delete": ["computador-com-problema", "checar-voltagem"]
 	},
     {
-		"action": "checar-circuito-de-alimentacao",
-		"preconds": ["computador-com-problema", "Computador-nao-liga", "fonte-inicia", "Voltagem-correta"],
-		"add": ["circuito-de-alimentacao"],
-		"delete": [" "]
-	},
-    {
-		"action": "Problema-no-circuito-de-alimentacao-da-placa-mae",
-		"preconds": ["computador-com-problema", "Computador-nao-liga", "fonte-inicia", "Voltagem-correta", "circuito-de-alimentacao"],
-		"add": ["Problema-detectado"],
-		"delete": ["computador-com-problema"]
-	},
-	{
-		"action": "o-computador-esta-ligado-na-tomada",
-		"preconds": ["computador-nao-liga","fonte-nao-inicia", "alimentacao-nao-conectada"],
-		"add": ["computador-liga"],
-		"delete": ["computador-nao-liga", "fonte-nao-inicia", "alimentacao-nao-conectada"]
-	},
-    {
-		"action": "fonte-inicia",
-		"preconds": ["computador-nao-liga", "fonte-nao-inicia"],
-		"add": ["alimentacao-conectada"],
-		"delete": [""]
+		"action": "voltagem-nao-esta-correta",
+		"preconds": ["computador-com-problema", "computador-nao-liga", "fonte-inicia",  "checar-voltagem", "voltagem-nao-esta-correta"],
+		"add": ["fonte-esta-com-problema"],
+		"delete": ["computador-com-problema", "checar-voltagem"]
 	},
     {
 		"action": "alimentacao-conectada",
-		"preconds": ["computador-nao-liga", "fonte-nao-inicia"],
-		"add": ["alimentacao-nao-conectada"],
-		"delete": [""]
+		"preconds": ["computador-com-problema", "computador-nao-liga", "fonte-nao-inicia", "checar-alimentacao", "alimentacao-conectada"],
+		"add": ["fonte-esta-queimada"],
+		"delete": ["computador-com-problema", "checar-alimentacao"]
 	},
-	 {
-		"action": "checar-se-o-computador-liga",
-		"preconds": ["computador-com-problema", "Computador-nao-liga"],
-		"add": ["fonte-inicia"],
-		"delete": [" "]
-	},
-   
+    
+    {
+		"action": "alimentacao-nao-conectada",
+		"preconds": ["computador-com-problema", "computador-nao-liga", "fonte-nao-inicia", "checar-alimentacao", "alimentacao-nao-conectada"],
+		"add": ["ligar-na-tomada"],
+		"delete": ["computador-com-problema", "checar-alimentacao"]
+	}, 
 	]
 }
 
