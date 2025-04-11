f4e3833a8176033b7eca41d856f466439e446352
Fritzhoy <camila.gmarques2@gmail.com>
2025-04-11 15:02:38 -0300

    alteraçoes texto açoes

--- exercicio/GERADOR_DE_PLANO.py
+++ exercicio/GERADOR_DE_PLANO.py
@@ -6,134 +6,134 @@ problem = {
     
 	"ops": [
     {
-		"action": "sistema-operacional-inicia-completamente",
+		"action": "Cheque se o sistema operacional inicia completamente",
 		"preconds": ["computador-com-problema", "a-bios-inicia", "computador-liga", "sistema-operacional-inicia", "checar-sistema-operacional-inicia-completamente", "sistema-operacional-inicia-completamente"],
 		"add": ["Operando-normalmente"],
 		"delete": ["computador-com-problema", "checar-sistema-operacional-inicia-completamente"]
 	},
     {
-		"action": "sistema-operacional-nao-inicia-completamente",
+		"action": "Cheque se o sistema operacional inicia completamente",
 		"preconds": ["computador-com-problema", "a-bios-inicia", "computador-liga", "sistema-operacional-inicia", "checar-sistema-operacional-inicia-completamente", "sistema-operacional-nao-inicia-completamente"],
 		"add": ["sistema-operacional-corrompido"],
 		"delete": ["computador-com-problema", "checar-sistema-operacional-inicia-completamente"]
 	},
     {
-		"action": "a-bios-inicia",
+		"action": "A bios inicia",
 		"preconds": ["computador-com-problema", "computador-liga", "checar-bios", "a-bios-inicia"],
 		"add": ["checar-OS"],
 		"delete": ["checar-bios"]
 	},
     {
-		"action": "a-bios-nao-inicia",
+		"action": "A bios inicia",
 		"preconds": ["computador-com-problema", "computador-liga", "checar-bios", "a-bios-nao-inicia"],
 		"add": ["checar-speaker"],
 		"delete": ["checar-bios"]
 	},
     {
-		"action": "sistema-operacional-nao-inicia",
+		"action": "O sistema operacional inicia",
 		"preconds": ["computador-com-problema", "computador-liga", "a-bios-inicia", "checar-OS", "sistema-operacional-nao-inicia"],
 		"add": ["checar-unidade-armazenamento"],
 		"delete": ["checar-OS"]
 	},
     {
-		"action": "sistema-operacional-inicia",
+		"action": "O sistema operacional inicia",
 		"preconds": ["computador-com-problema", "computador-liga", "a-bios-inicia", "checar-OS", "sistema-operacional-inicia"],
 		"add": ["checar-sistema-operacional-inicia-completamente"],
 		"delete": ["checar-OS"]
 	},
     {
-		"action": "unidade-de-armazenamento-detectada",
+		"action": "Unidade de armazenamento",
 		"preconds": ["computador-com-problema", "computador-liga", "a-bios-inicia", "sistema-operacional-nao-inicia", "unidade-de-armazenamento-detectada", "checar-unidade-armazenamento"],
 		"add": ["checar-ordem-boot"],
 		"delete": ["checar-unidade-armazenamento"]
 	},
     {
-		"action": "unidade-de-armazenamento-nao-detectada",
+		"action": "Unidade de armazenamento",
 		"preconds": ["computador-com-problema", "computador-liga", "a-bios-inicia", "sistema-operacional-nao-inicia", "unidade-de-armazenamento-nao-detectada", "checar-unidade-armazenamento"],
 		"add": ["HD-desconectado-ou-com-problema"],
 		"delete": ["computador-com-problema", "checar-unidade-armazenamento"]
 	},
     {
-		"action": "ordem-de-boot-correta",
+		"action": "A ordem de boot está correta",
 		"preconds": ["computador-com-problema", "computador-liga", "a-bios-inicia", "sistema-operacional-nao-inicia", "unidade-de-armazenamento-detectada", "checar-ordem-boot", "ordem-de-boot-correta"],
 		"add": ["checar-unidade-de-armazenamento-integra"],
 		"delete": ["checar-ordem-boot"]
 	},
     {
-		"action": "ordem-de-boot-nao-esta-correta",
+		"action": "A ordem de boot está correta",
 		"preconds": ["computador-com-problema", "computador-liga", "a-bios-inicia", "sistema-operacional-nao-inicia", "unidade-de-armazenamento-detectada", "checar-ordem-boot", "ordem-de-boot-nao-esta-correta"],
 		"add": ["ajustar-boot"],
 		"delete": ["computador-com-problema", "checar-ordem-boot"]
 	},
     {
-		"action": "unidade-de-armazenamento-nao-integra",
+		"action": "Unidade de armazenamento está integra",
 		"preconds": ["computador-com-problema", "computador-liga", "a-bios-inicia", "sistema-operacional-nao-inicia", "unidade-de-armazenamento-detectada", "ordem-de-boot-correta", "checar-unidade-de-armazenamento-integra", "unidade-de-armazenamento-nao-integra"],
 		"add": ["troque-o-HD/SDD"],
 		"delete": ["checar-unidade-de-armazenamento-integra", "computador-com-problema"]
 	},
     {
-		"action": "unidade-de-armazenamento-integra",
+		"action": "Unidade de armazenamento está integra",
 		"preconds": ["computador-com-problema", "computador-liga", "a-bios-inicia", "sistema-operacional-nao-inicia", "unidade-de-armazenamento-detectada", "ordem-de-boot-correta", "checar-unidade-de-armazenamento-integra", "unidade-de-armazenamento-integra"],
 		"add": ["sistema-operacional-corrompido"],
 		"delete": ["checar-unidade-de-armazenamento-integra", "computador-com-problema"]
 	},
     {
-		"action": "Speaker-nao-emite-alerta",
+		"action": "Speaker emite alerta",
 		"preconds": ["computador-com-problema", "computador-liga", "a-bios-nao-inicia", "checar-speaker", "Speaker-nao-emite-alerta"],
 		"add": ["problema-na-placa-mae"],
 		"delete": ["computador-com-problema", "checar-speaker"]
 	},
     {
-		"action": "Speaker-emite-alerta",
+		"action": "Speaker emite alerta",
 		"preconds": ["computador-com-problema", "computador-liga", "a-bios-nao-inicia", "checar-speaker", "Speaker-emite-alerta"],
 		"add": ["problema-processador-ou-memoria-ram"],
 		"delete": ["computador-com-problema", "checar-speaker"]
 	},
     {
-		"action": "o-computador-liga",
+		"action": "O computador liga",
 		"preconds": ["computador-com-problema", "computador-liga"],
 		"add": ["checar-bios"],
 		"delete": ["checar-computador-liga"]
 	},
     {
-		"action": "o-computador-nao-liga",
+		"action": "O computador liga",
 		"preconds": ["computador-com-problema", "computador-nao-liga"],
 		"add": ["checar-fonte"],
 		"delete": [" "]
 	},
     {
-		"action": "fonte-inicia",
+		"action": "A fonte inicia",
 		"preconds": ["computador-nao-liga", "checar-fonte","fonte-inicia"],
 		"add": ["checar-voltagem"],
 		"delete": ["checar-fonte"]
 	},
     {
-		"action": "fonte-nao-inicia",
+		"action": "A fonte inicia",
 		"preconds": ["computador-com-problema", "computador-nao-liga", "checar-fonte",  "fonte-nao-inicia"],
 		"add": ["checar-alimentacao"],
 		"delete": ["checar-fonte"]
 	},
     {
-		"action": "voltagem-correta",
+		"action": "Voltagem está correta",
 		"preconds": ["computador-com-problema", "computador-nao-liga", "fonte-inicia",  "checar-voltagem", "voltagem-correta"],
 		"add": ["problema-no-circuito-da-placa-mae"],
 		"delete": ["computador-com-problema", "checar-voltagem"]
 	},
     {
-		"action": "voltagem-nao-esta-correta",
+		"action": "Voltagem está correta",
 		"preconds": ["computador-com-problema", "computador-nao-liga", "fonte-inicia",  "checar-voltagem", "voltagem-nao-esta-correta"],
 		"add": ["fonte-esta-com-problema"],
 		"delete": ["computador-com-problema", "checar-voltagem"]
 	},
     {
-		"action": "alimentacao-conectada",
+		"action": "Alimentação está contectada",
 		"preconds": ["computador-com-problema", "computador-nao-liga", "fonte-nao-inicia", "checar-alimentacao", "alimentacao-conectada"],
 		"add": ["fonte-esta-queimada"],
 		"delete": ["computador-com-problema", "checar-alimentacao"]
 	},
     
     {
-		"action": "alimentacao-nao-conectada",
+		"action": "Alimentação está contectada",
 		"preconds": ["computador-com-problema", "computador-nao-liga", "fonte-nao-inicia", "checar-alimentacao", "alimentacao-nao-conectada"],
 		"add": ["ligar-na-tomada"],
 		"delete": ["computador-com-problema", "checar-alimentacao"]
@@ -144,7 +144,7 @@ def Lista_Problemas():
     print("================================================================================")
     print("\nLista de Problemas:\n")
     print("1 - Operando-normalmente")
-    print("2 - Sistema operacional corrompido")
+    print("2 - Reinstalar sistema operacional")
     print("3 - Sistema operacional corrompido")
     print("4 - Troque o HD/SDD")
     print("5 - Ajustar boot")
@@ -187,7 +187,7 @@ def Lista_Problemas():
                "sistema-operacional-nao-inicia", "unidade-de-armazenamento-nao-detectada"],
               ["HD-desconectado-ou-com-problema"]),
 
-        "7": (["computador-com-problema", "checar-computador-liga",
+        "7": (["computador-com-problema", "computador-liga",
                "a-bios-nao-inicia", "Speaker-emite-alerta"],
               ["problema-processador-ou-memoria-ram"]),
 
@@ -223,7 +223,7 @@ def main():
 
     start, finish = Lista_Problemas()
     ops = problem['ops']
-    msg = 'Deve-se executar:  '
+    msg = 'Checar-se:  '
     plan = gps(start, finish, ops, msg)
     if plan is not None:
         for action in plan:
